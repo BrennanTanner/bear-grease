18b27c8136f27094f92db60081be2b88e5fa44d9
BrennanTanner <brenntanner@gmail.com>
2023-04-26 19:55:02 -0600

    added test firebase function

--- frontend/functions/index.js
+++ frontend/functions/index.js
@@ -1,9 +1,25 @@
 const functions = require("firebase-functions");
-
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.helloWorld = functions.https.onRequest((req, res) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+});
+
+exports.catalog = functions.https.onRequest(async (req, res) => {
+  const headersList = {
+    Accept: "*/*",
+    Authorization: "Bearer bqnqQTtPBz3rCOUwWR3uKg93NOgoKOEja87q7GQW",
+  };
+
+  const response = await fetch("https://api.printful.com/store/products", {
+    method: "GET",
+    headers: headersList,
+  });
+  const catalog = await response.json();
+  console.log(catalog);
+  if (!catalog) {
+    return res.sendStatus(404);
+  }
+  res.json(catalog);
 });
