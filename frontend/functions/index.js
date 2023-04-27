073f79057bb2e8d7edcd89168e5f4df4520fa9a2
BrennanTanner <brenntanner@gmail.com>
2023-04-26 20:21:08 -0600

    updated firebase hosting instruction

--- frontend/functions/index.js
+++ frontend/functions/index.js
@@ -1,25 +1,39 @@
-const functions = require("firebase-functions");
+/* eslint-disable object-curly-spacing */
+/* eslint-disable indent */
+/* eslint-disable quotes */
+const functions = require('firebase-functions');
+
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
-
+// delete this
 exports.helloWorld = functions.https.onRequest((req, res) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+   functions.logger.info('Hello logs!', { structuredData: true });
 });
 
+// call printfull api
 exports.catalog = functions.https.onRequest(async (req, res) => {
-  const headersList = {
-    Accept: "*/*",
-    Authorization: "Bearer bqnqQTtPBz3rCOUwWR3uKg93NOgoKOEja87q7GQW",
-  };
+   const headersList = {
+      Accept: '*/*',
+      Authorization: 'Bearer bqnqQTtPBz3rCOUwWR3uKg93NOgoKOEja87q7GQW',
+   };
 
-  const response = await fetch("https://api.printful.com/store/products", {
-    method: "GET",
-    headers: headersList,
-  });
-  const catalog = await response.json();
-  console.log(catalog);
-  if (!catalog) {
-    return res.sendStatus(404);
-  }
-  res.json(catalog);
+   await fetch('https://api.printful.com/store/products', {
+      method: 'GET',
+      headers: headersList,
+   })
+      .then((resp) => {
+         if (resp.status >= 200 && resp.status <= 300) {
+            console.log('hi');
+            return Promise.resolve(resp.json());
+         }
+      })
+      .then((data) => {
+         console.log(data);
+      });
+
+   // const catalog = await res.json();
+   // if (!catalog) {
+   //   return res.sendStatus(404);
+   // }
+   // res.json(catalog);
 });
