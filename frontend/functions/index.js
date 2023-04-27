1c6197cfc0d15a33224d08cc4efe95c7ff59ce9a
BrennanTanner <brenntanner@gmail.com>
2023-04-27 17:58:22 -0600

    removed backend/fix to firebase hosting

--- frontend/functions/index.js
+++ frontend/functions/index.js
@@ -1,20 +1,21 @@
+/* eslint-disable max-len */
 /* eslint-disable object-curly-spacing */
 /* eslint-disable indent */
 /* eslint-disable quotes */
 const functions = require('firebase-functions');
+const { defineString } = require('firebase-functions/params');
+
+// Define ENV params
+const printfulApi = defineString('PRINTFUL_API_TOKEN');
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
-// delete this
-exports.helloWorld = functions.https.onRequest((req, res) => {
-   functions.logger.info('Hello logs!', { structuredData: true });
-});
 
-// call printfull api
+// Gets entire store catalog
 exports.catalog = functions.https.onRequest(async (req, res) => {
    const headersList = {
       Accept: '*/*',
-      Authorization: 'Bearer bqnqQTtPBz3rCOUwWR3uKg93NOgoKOEja87q7GQW',
+      Authorization: `Bearer ${printfulApi.value()}`,
    };
 
    await fetch('https://api.printful.com/store/products', {
@@ -23,17 +24,94 @@ exports.catalog = functions.https.onRequest(async (req, res) => {
    })
       .then((resp) => {
          if (resp.status >= 200 && resp.status <= 300) {
-            console.log('hi');
             return Promise.resolve(resp.json());
          }
       })
       .then((data) => {
-         console.log(data);
+         res.json(data);
       });
+});
 
-   // const catalog = await res.json();
-   // if (!catalog) {
-   //   return res.sendStatus(404);
-   // }
-   // res.json(catalog);
+// Gets items by category
+exports.category = functions.https.onRequest(async (req, res) => {
+  const headersList = {
+     Accept: '*/*',
+     Authorization: `Bearer ${printfulApi.value()}`,
+  };
+
+  await fetch(`https://api.printful.com/store/products?category_id=${req.params[0]}`, {
+     method: 'GET',
+     headers: headersList,
+  })
+     .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 300) {
+           return Promise.resolve(resp.json());
+        }
+     })
+     .then((data) => {
+        res.json(data);
+     });
+});
+
+// Gets all printful categories
+exports.categories = functions.https.onRequest(async (req, res) => {
+   const headersList = {
+      Accept: '*/*',
+      Authorization: `Bearer ${printfulApi.value()}`,
+   };
+
+   await fetch('https://api.printful.com/store/categories', {
+      method: 'GET',
+      headers: headersList,
+   })
+      .then((resp) => {
+         if (resp.status >= 200 && resp.status <= 300) {
+            return Promise.resolve(resp.json());
+         }
+      })
+      .then((data) => {
+         res.json(data);
+      });
+});
+
+// Gets product information
+exports.product = functions.https.onRequest(async (req, res) => {
+   const headersList = {
+      Accept: '*/*',
+      Authorization: `Bearer ${printfulApi.value()}`,
+   };
+
+   await fetch(`https://api.printful.com/store/products/${req.params[0]}`, {
+      method: 'GET',
+      headers: headersList,
+   })
+      .then((resp) => {
+         if (resp.status >= 200 && resp.status <= 300) {
+            return Promise.resolve(resp.json());
+         }
+      })
+      .then((data) => {
+         res.json(data);
+      });
+});
+
+// Gets product information
+exports.productTest = functions.https.onRequest(async (req, res) => {
+  const headersList = {
+     Accept: '*/*',
+     Authorization: `Bearer ${printfulApi.value()}`,
+  };
+
+  await fetch(`https://api.printful.com/store/products/306499346`, {
+     method: 'GET',
+     headers: headersList,
+  })
+     .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 300) {
+           return Promise.resolve(resp.json());
+        }
+     })
+     .then((data) => {
+        res.json(data);
+     });
 });
